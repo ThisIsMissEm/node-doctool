8e45122c423f4e65e18b1c83b184458f96d318e3
Micheil Smith <micheil@brandedcode.com>
2011-03-31 03:12:48 +1100

    Adding in stats

--- sample/run-sample.js
+++ sample/run-sample.js
@@ -5,3 +5,7 @@ var sys = require('sys');
 var file = fs.readFileSync(process.cwd() + '/sample/fs.nd', 'utf8');
 var doc = new Parser(file);
 doc.parse();
+
+//console.log(doc.warnings);
+
+console.log(doc.statistics());
